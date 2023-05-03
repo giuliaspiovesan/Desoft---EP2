7db1be05a79ff4874cee182a54c6b53e91fd3024
giuliaspiovesan <giuliaspiovesan@outlook.com>
2023-05-03 08:27:34 -0300

    preenche frota

--- EP2.py
+++ EP2.py
@@ -9,3 +9,10 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             posicoes.append([linha + a, coluna])
     return posicoes          
 
+#Preenche frota:
+def preenche_frota(dicio_frota, nome_navio, linha_navio, coluna_navio, orientacao_navio, tamanho_navio):
+    if nome_navio not in dicio_frota:
+        dicio_frota[nome_navio] = [define_posicoes(linha_navio, coluna_navio, orientacao_navio, tamanho_navio)]
+    else:
+        dicio_frota[nome_navio].append(define_posicoes(linha_navio, coluna_navio, orientacao_navio, tamanho_navio))
+    return dicio_frota
